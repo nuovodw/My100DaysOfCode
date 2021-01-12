847a31216e08ad34ab9a963d4cc3e18ef474274b
nuovodw <68261298+nuovodw@users.noreply.github.com>
2021-01-12 06:30:55 -0600

    Update script.js

--- Code Practice/Pig_Game/script.js
+++ Code Practice/Pig_Game/script.js
@@ -1,5 +1,7 @@
 'use strict';
 //Selecting elements
+const player0E1 = document.querySelector('.player--0');
+const player1E1 = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
@@ -14,7 +16,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden'); //Hide the die
 
+const scores = [0, 0]; //store the scores as they get added
 let currentScore = 0; //cannot be inside the function; otherwise it would be set to 0 when we reset
+let activePlayer = 0; //0 identifies player 1; 1 identifies player 2
 
 //rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -29,8 +33,19 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore = currentScore + dice;
-    current0El.textContent = currentScore; //change later
+    //switch players by manipulating the id current--0/current--1
+    // current0El.textContent = currentScore; // make *current0El dynamic
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0; //if the active player is player 0, then we want the new active player to be player 1; else it should be player 0
+
+    //switch background color by toggling the .player--acive class
+    player0E1.classList.toggle('player--active');
+    player1E1.classList.toggle('player--active');
   }
 });
