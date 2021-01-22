0f43ac49b3f025a5122b0eb15d7c9058be17c7a2
nuovodw <68261298+nuovodw@users.noreply.github.com>
2021-01-21 21:00:21 -0600

    Update script.js

--- Code Practice/Dicee_Game/script.js
+++ Code Practice/Dicee_Game/script.js
@@ -1,19 +1,18 @@
 'use strict';
 let randomNumber1 = Math.floor(Math.random() * 6 + 1); //1-6
+let randomNumber2 = Math.floor(Math.random() * 6 + 1);
 let randomDiceImage = 'dice' + randomNumber1 + '.png'; //dice images
 let randomImageSource = 'images/' + randomDiceImage; //dice images from folder
-
+let randomImageSource2 = 'images/dice' + randomNumber2 + '.png';
 let image1 = document.querySelectorAll('img')[0];
 image1.setAttribute('src', randomImageSource);
-
-let randomNumber2 = Math.floor(Math.random() * 6 + 1);
-let randomImageSource2 = 'images/dice' + randomNumber2 + '.png';
 document.querySelectorAll('img')[1].setAttribute('src', randomImageSource2);
+const gameTitle = document.querySelector('h1');
 
 if (randomNumber1 > randomNumber2) {
-	document.querySelector('h1').innerHTML = 'Player 1 wins! 🚩';
+	gameTitle.innerHTML = 'Player 1 wins! 🚩';
 } else if (randomNumber1 < randomNumber2) {
-	document.querySelector('h1').innerHTML = 'Player 2 wins! 🚩';
+	gameTitle.innerHTML = 'Player 2 wins! 🚩';
 } else {
-	document.querySelector('h1').innerHTML = 'Draw';
+	gameTitle.innerHTML = 'Draw';
 }
