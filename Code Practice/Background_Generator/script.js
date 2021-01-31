db1b936d14a14638ea7b033eecf24a37dd376b93
nuovodw <68261298+nuovodw@users.noreply.github.com>
2021-01-31 08:16:24 -0600

    add random function

--- Code Practice/Background_Generator/script.js
+++ Code Practice/Background_Generator/script.js
@@ -2,7 +2,10 @@
 const css = document.querySelector('h3');
 const color1 = document.querySelector('.color1');
 const color2 = document.querySelector('.color2');
+const colorRandom = document.querySelector('.color_random');
 const body = document.getElementById('gradient');
+const randomColor1 = Math.floor(Math.random() * 16777215).toString(16);
+const randomColor2 = Math.floor(Math.random() * 16777215).toString(16);
 
 function setGradient() {
 	body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
@@ -10,6 +13,15 @@ function setGradient() {
 	css.textContent = body.style.background + ';';
 }
 
+function setRandomGradient() {
+	const hex1 = randomColor1;
+	const hex2 = randomColor2;
+	body.style.background = `linear-gradient(to right, #${hex1}, #${hex2})`;
+	css.textContent = body.style.background + ';';
+}
+
 color1.addEventListener('input', setGradient);
 
 color2.addEventListener('input', setGradient);
+
+colorRandom.addEventListener('click', setRandomGradient);
