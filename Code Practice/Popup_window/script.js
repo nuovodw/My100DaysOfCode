d464417bf4e0261fe77c41cb103e2deec49eccba
nuovodw <68261298+nuovodw@users.noreply.github.com>
2021-02-23 20:37:20 -0600

    add js and css

---
+++ Code Practice/Popup_window/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const button = document.querySelector('button');
+const popup = document.querySelector('.popup-wrapper');
+const close = document.querySelector('.popup-close');
+
+button.addEventListener('click', () => {
+	popup.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+	popup.style.display = 'none';
+});
+
+popup.addEventListener('click', () => {
+	popup.style.display = 'none';
+});
